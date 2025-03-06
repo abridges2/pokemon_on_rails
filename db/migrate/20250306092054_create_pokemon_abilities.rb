b4a442d58c00c95dbd917600c4214fdbed00a02b
abridges2 <aidan.bridges@shaw.ca>
2025-03-06 03:21:23 -0600

    Generated the PokemonAbility model for the join table between Pokemons and Abilities.

---
+++ db/migrate/20250306092054_create_pokemon_abilities.rb
@@ -0,0 +1,11 @@
+class CreatePokemonAbilities < ActiveRecord::Migration[8.0]
+  def change
+    create_table :pokemon_abilities do |t|
+      t.references :pokemon, null: false, foreign_key: true
+      t.references :ability, null: false, foreign_key: true
+      t.boolean :is_hidden
+
+      t.timestamps
+    end
+  end
+end
