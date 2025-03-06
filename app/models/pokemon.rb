ab229b48a24c1ca037d1f4232058d6e7d7aeedcb
abridges2 <aidan.bridges@shaw.ca>
2025-03-06 04:02:08 -0600

    Added validations to the Pokemon Model class to ensure all fields are present, and name and sprite_url are unique to each Pokemon.

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon < ApplicationRecord
+  has_many :pokemon_types
+  has_many :types, through: :pokemon_types
+
+  has_many :pokemon_abilities
+  has_many :abilities, through: :pokemon_abilities
+
+  has_many :pokemon_moves
+  has_many :moves, through: :pokemon_moves
+
+  validates :name, :height, :weight, :base_experience, :species, :sprite_url,  presence: true
+
+  validates :name, uniqueness: true
+  validates :sprite_url, uniqueness: true
 end
